220e1a043615c1910e713eac8666343002cd2087
ITxxxxMChandirasekaran <manjula.chandirasekaran@tri-c.edu>
2018-10-31 17:39:09 -0400

    Initial

---
+++ Ajax MVC startup files/BookStore/Scripts/Apps/myScript.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+    /*
+    ../Books - shows list of Books
+    ../ Authors - shows list of Authors
+    
+    ../Books/Search - returns a JSON array of all books written by the given Author
+        -This is a GET method
+        -Input parameter is a string (Author name)
+        -This method returns a JSON
+    
+    ../Books/DeleteByAuthor - returns a JSOn object showing count and status of books deleted 
+    - This is a POST method
+    - Input parameter is a string (Author name)
+    -This method returns a JSON
+
+    */
+
+    
+
+});
